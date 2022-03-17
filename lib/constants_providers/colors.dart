0fcc1892b28a87afbda22d650e33273fa2456012
Omid <omidomirzaei@gmail.com>
2022-03-17 13:05:29 +0330

    colors added

---
+++ lib/constants_providers/colors.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+
+// => The Color Name On Figma
+class COLORS {
+  //Heyo Brand/Green
+  static const Color kGreenMainColor = Color(0xff02AEA4);
+  //Heyo Brand/Green Light
+  static const Color kGreenLighterColor = Color(0xffE9FBF8);
+
+  //CorePass/CorePass Blue
+  static const Color kBlueMainColor = Color(0xff2F80ED);
+  //Heyo Brand/Dark Blue
+  static const Color kDarkBlueColor = Color(0xff1E294C);
+  //Heyo Brand/Bright Blue
+  static const Color kBrightBlueColor = Color(0xffF8F7FF);
+  //Heyo Brand/Background
+  static const Color kAppBackground = Color(0xffFFFFFF);
+
+  //Monochrome/ Text Black
+  static const Color kTextBlackColor = Color(0xff1A1A1A);
+  //Heyo Brand/Text Soft Blue
+  static const Color kTextSoftBlueColor = Color(0xff697087);
+
+  //States/Info
+  static const Color kStatesBlueColor = Color(0xff073ABD);
+  //States/Info Light
+  static const Color kStatesLightBlueColor = Color(0xffE7F6FF);
+  //States/Error
+  static const Color kStatesRedColor = Color(0xffE81539);
+  //States/Warning
+  static const Color kStatesYellowColor = Color(0xffF2BD02);
+  //States/Warning Light
+  static const Color kStatesYellowLightColor = Color(0xffFFFAE7);
+}
