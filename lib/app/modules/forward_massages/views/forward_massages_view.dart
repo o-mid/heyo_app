d6db4727de3861af072c253b2702e9d3b1c45f20
Omid <omidomirzaei@gmail.com>
2022-04-27 16:04:00 +0430

    fixed Overflow - refactoring

--- lib/app/modules/forward_massages/views/forward_massages_view.dart
+++ lib/app/modules/forward_massages/views/forward_massages_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
+import 'package:heyo/app/modules/forward_massages/widgets/contacts_widget.dart';
+import 'package:heyo/app/modules/new_chat/data/models/user_model.dart';
 import 'package:heyo/app/modules/shared/utils/constants/colors.dart';
 import 'package:heyo/app/modules/shared/utils/constants/textStyles.dart';
 import 'package:heyo/app/modules/shared/utils/screen-utils/inputs/custom_text_field.dart';
@@ -11,178 +13,79 @@ import '../../../../generated/locales.g.dart';
 import '../../new_chat/widgets/user_widget.dart';
 import '../../shared/widgets/list_header_widget.dart';
 import '../controllers/forward_massages_controller.dart';
+import '../widgets/recent_contacts_widget.dart';
 
 class ForwardMassagesView extends GetView<ForwardMassagesController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: Text(
-          LocaleKeys.forwardMassagesPage_appbar.tr,
-          style: TEXTSTYLES.kHeaderLarge.copyWith(color: COLORS.kWhiteColor),
-        ),
-        leading: IconButton(
-          onPressed: (() => Get.back()),
-          icon: const Icon(
-            Icons.arrow_back,
-            color: COLORS.kWhiteColor,
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text(
+            LocaleKeys.forwardMassagesPage_appbar.tr,
+            style: TEXTSTYLES.kHeaderLarge.copyWith(color: COLORS.kWhiteColor),
           ),
+          leading: IconButton(
+            onPressed: (() => Get.back()),
+            icon: const Icon(
+              Icons.arrow_back,
+              color: COLORS.kWhiteColor,
+            ),
+          ),
+          centerTitle: false,
+          automaticallyImplyLeading: true,
+          backgroundColor: COLORS.kGreenMainColor,
         ),
-        centerTitle: false,
-        automaticallyImplyLeading: true,
-        backgroundColor: COLORS.kGreenMainColor,
-      ),
-      backgroundColor: COLORS.kWhiteColor,
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Column(
-                  children: [
-                    CustomSizes.largeSizedBoxHeight,
-                    Padding(
-                      padding: CustomSizes.contentPaddingWidth,
-                      child: FocusScope(
-                        child: Focus(
-                          onFocusChange: (focus) =>
-                              controller.isTextInputFocused.value = focus,
-                          child: CUSTOMTEXTFIELD(
-                            textController: controller.inputController,
-                            labelText:
-                                LocaleKeys.forwardMassagesPage_textInput.tr,
-                          ),
+        backgroundColor: COLORS.kWhiteColor,
+        body: Obx(() {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Column(
+                children: [
+                  CustomSizes.largeSizedBoxHeight,
+                  Padding(
+                    padding: CustomSizes.contentPaddingWidth,
+                    child: FocusScope(
+                      child: Focus(
+                        onFocusChange: (focus) =>
+                            controller.isTextInputFocused.value = focus,
+                        child: CUSTOMTEXTFIELD(
+                          textController: controller.inputController,
+                          labelText:
+                              LocaleKeys.forwardMassagesPage_textInput.tr,
                         ),
                       ),
                     ),
-                    CustomSizes.largeSizedBoxHeight,
-                    Divider(
-                      color: COLORS.kBrightBlueColor,
-                      thickness: 8,
-                    ),
-                  ],
+                  ),
+                  CustomSizes.largeSizedBoxHeight,
+                  Divider(
+                    color: COLORS.kBrightBlueColor,
+                    thickness: 8,
+                  ),
+                ],
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      //dont show recent Contacts when the input is focused and its searching for users
+                      controller.isTextInputFocused.value
+                          ? SizedBox()
+                          : recentContactsWidget(users: controller.users),
+                      contactsWidget(
+                        isTextInputFocused: controller.isTextInputFocused,
+                        searchSuggestions: controller.searchSuggestions,
+                      ),
+                      CustomSizes.largeSizedBoxHeight,
+                    ],
+                  ),
                 ),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    CustomSizes.largeSizedBoxHeight,
-                    Padding(
-                      padding: CustomSizes.contentPaddingWidth,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            LocaleKeys.forwardMassagesPage_recentContacts.tr,
-                            style: TEXTSTYLES.kLinkSmall
-                                .copyWith(color: COLORS.kTextBlueColor),
-                          ),
-                          CustomSizes.smallSizedBoxHeight,
-                          ListView.builder(
-                            itemCount: controller.users.length,
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemBuilder: (BuildContext context, int index) {
-                              return Padding(
-                                padding: EdgeInsets.only(
-                                  bottom: 8.h,
-                                  top: 8.h,
-                                ),
-                                child: InkWell(
-                                  borderRadius: BorderRadius.circular(8),
-                                  onTap: () {},
-                                  child:
-                                      UserWidget(User: controller.users[index]),
-                                ),
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                    Divider(
-                      color: COLORS.kBrightBlueColor,
-                      thickness: 8,
-                    ),
-                    CustomSizes.largeSizedBoxHeight,
-                    Padding(
-                      padding: CustomSizes.contentPaddingWidth,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            LocaleKeys.forwardMassagesPage_OtherContacts.tr,
-                            style: TEXTSTYLES.kLinkSmall
-                                .copyWith(color: COLORS.kTextBlueColor),
-                          ),
-                          Obx(() {
-                            return ListView.builder(
-                              itemCount: controller.searchSuggestions.length,
-                              shrinkWrap: true,
-                              physics: const NeverScrollableScrollPhysics(),
-                              itemBuilder: (BuildContext context, int index) {
-                                //this will grab the current user and
-                                // extract the first character from its name
-                                String _currentUsernamefirstchar = controller
-                                    .searchSuggestions[index]
-                                    .name
-                                    .characters
-                                    .first;
-                                //this will grab the next user in the list if its not null and
-                                // extract the first character from its name
-                                String _nextUsernamefirstchar = controller
-                                            .searchSuggestions
-                                            .indexOf(controller
-                                                .searchSuggestions.last) >
-                                        index + 1
-                                    ? controller.searchSuggestions[index + 1]
-                                        .name.characters.first
-                                    : "";
-                                return Padding(
-                                  padding: EdgeInsets.only(
-                                    bottom: 8.h,
-                                    top: 8.h,
-                                  ),
-                                  child: Column(
-                                    children: [
-                                      _currentUsernamefirstchar !=
-                                              _nextUsernamefirstchar
-                                          ? ListHeaderWidget(
-                                              title: controller
-                                                  .searchSuggestions[index]
-                                                  .name
-                                                  .characters
-                                                  .first
-                                                  .toUpperCase())
-                                          : const SizedBox(),
-                                      InkWell(
-                                        borderRadius: BorderRadius.circular(8),
-                                        onTap: () {},
-                                        child: UserWidget(
-                                            User: controller
-                                                .searchSuggestions[index]),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              },
-                            );
-                          }),
-                        ],
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+              )
+            ],
+          );
+        }));
   }
 }
