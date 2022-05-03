5af1676e47f24a53546eeebf0082aacab1354807
Ramin R <ramin.r9877@gmail.com>
2022-05-03 12:28:11 +0430

    added glassmorphism container

--- lib/app/modules/messages/widgets/body/message_selection_wrapper.dart
+++ lib/app/modules/messages/widgets/body/message_selection_wrapper.dart
@@ -6,6 +6,7 @@ import 'package:heyo/app/modules/messages/data/models/messages/audio_message_mod
 import 'package:heyo/app/modules/messages/data/models/messages/message_model.dart';
 import 'package:heyo/app/modules/messages/data/models/messages/video_message_model.dart';
 import 'package:heyo/app/modules/shared/utils/constants/colors.dart';
+import 'package:heyo/app/modules/shared/widgets/glassmorphic_container.dart';
 
 import 'message_from_me_widget.dart';
 import 'message_from_other_widget.dart';
@@ -18,8 +19,7 @@ class MessageSelectionWrapper extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<MessageSelectionWrapper> createState() =>
-      _MessageSelectionWrapperState();
+  State<MessageSelectionWrapper> createState() => _MessageSelectionWrapperState();
 }
 
 class _MessageSelectionWrapperState extends State<MessageSelectionWrapper>
@@ -38,13 +38,13 @@ class _MessageSelectionWrapperState extends State<MessageSelectionWrapper>
           ? null
           : () => controller.toggleMessageSelection(message.messageId),
       child: Stack(
+        clipBehavior: Clip.none,
+        alignment: Alignment.center,
         children: [
           // Material is used because if container is given color, it will
           // hide the reaction widget borders
           Material(
-            color: message.isSelected
-                ? COLORS.kGreenLighterColor
-                : Colors.transparent,
+            color: message.isSelected ? COLORS.kGreenLighterColor : Colors.transparent,
             child: Container(
               padding: EdgeInsets.symmetric(vertical: 4.h),
               child: message.isFromMe
@@ -57,10 +57,37 @@ class _MessageSelectionWrapperState extends State<MessageSelectionWrapper>
                     ),
             ),
           ),
+          // Todo: optimize if necessary
+          Positioned(
+            top: -4.h,
+            child: AnimatedScale(
+              scale: message.isSelected && controller.selectedMessages.length == 1 ? 1 : 0,
+              duration: const Duration(milliseconds: 200),
+              child: GlassmorphicContainer(
+                width: 315.w,
+                height: 64.h,
+                borderRadius: 8.r,
+                blur: 10,
+                border: 0,
+                linearGradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    const Color(0xFF466087).withOpacity(0.1),
+                    const Color(0xFF466087).withOpacity(0.05),
+                  ],
+                ),
+                child: Container(
+                  color: Colors.white.withOpacity(0.62),
+                ),
+              ),
+            ),
+          ),
           if (message.isSelected)
             Positioned(
               top: 0,
               bottom: 0,
+              left: 0,
               child: Container(
                 color: COLORS.kGreenMainColor,
                 width: 3,
@@ -73,6 +100,5 @@ class _MessageSelectionWrapperState extends State<MessageSelectionWrapper>
 
   @override
   bool get wantKeepAlive =>
-      widget.message is VideoMessageModel ||
-      widget.message is AudioMessageModel;
+      widget.message is VideoMessageModel || widget.message is AudioMessageModel;
 }
