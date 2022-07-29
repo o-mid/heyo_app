266e578184967dec574f3e87ec4a02652a2b7e44
Ramin <Ramin.r9877@gmail.com>
2022-07-29 14:38:27 +0430

    added toJson and fromJson to CallModel and UserModel

--- lib/app/modules/calls/shared/data/models/call_model.dart
+++ lib/app/modules/calls/shared/data/models/call_model.dart
@@ -15,6 +15,14 @@ enum CallStatus {
 }
 
 class CallModel {
+  static const idSerializedName = 'id';
+  static const typeSerializedName = 'type';
+  static const statusSerializedName = 'status';
+  static const dateSerializedName = 'date';
+  static const userSerializedName = 'user';
+  static const durationSerializedName = 'duration';
+  static const dataUsageMBSerializedName = 'dataUsageMB';
+
   final String id;
   final CallType type;
   final CallStatus status;
@@ -32,4 +40,24 @@ class CallModel {
     this.duration = Duration.zero,
     this.dataUsageMB = 0,
   });
+
+  factory CallModel.fromJson(Map<String, dynamic> json) => CallModel(
+        id: json[idSerializedName],
+        type: CallType.values.byName(json[typeSerializedName]),
+        status: CallStatus.values.byName(json[statusSerializedName]),
+        date: DateTime.parse(json[dateSerializedName]),
+        user: UserModel.fromJson(json[userSerializedName]),
+        duration: Duration(milliseconds: json[durationSerializedName]),
+        dataUsageMB: json[dataUsageMBSerializedName],
+      );
+
+  Map<String, dynamic> toJson() => {
+        idSerializedName: id,
+        typeSerializedName: type.name,
+        statusSerializedName: status.name,
+        dateSerializedName: date.toIso8601String(),
+        userSerializedName: user.toJson(),
+        durationSerializedName: duration.inMilliseconds,
+        dataUsageMBSerializedName: dataUsageMB,
+      };
 }
--- lib/app/modules/new_chat/data/models/user_model.dart
+++ lib/app/modules/new_chat/data/models/user_model.dart
@@ -1,6 +1,13 @@
 import 'package:heyo/app/modules/chats/data/models/chat_model.dart';
 
 class UserModel {
+  static const nameSerializedName = 'name';
+  static const iconSerializedName = 'icon';
+  static const walletAddressSerializedName = 'walletAddress';
+  static const nicknameSerializedName = 'nickname';
+  static const isVerifiedSerializedName = 'isVerified';
+  static const isContactSerializedName = 'isContact';
+
   String name;
   String icon;
   String walletAddress;
@@ -20,4 +27,22 @@ class UserModel {
     this.nickname = "",
     this.chatModel,
   });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        name: json[nameSerializedName],
+        icon: json[iconSerializedName],
+        walletAddress: json[walletAddressSerializedName],
+        nickname: json[nicknameSerializedName],
+        isVerified: json[isVerifiedSerializedName],
+        isContact: json[isContactSerializedName],
+      );
+
+  Map<String, dynamic> toJson() => {
+        nameSerializedName: name,
+        iconSerializedName: icon,
+        walletAddressSerializedName: walletAddress,
+        nicknameSerializedName: nickname,
+        isVerifiedSerializedName: isVerified,
+        isContactSerializedName: isContact,
+      };
 }
