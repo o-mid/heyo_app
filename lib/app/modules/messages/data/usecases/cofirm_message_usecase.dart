c3936ee7d3392be43b35c53cf0ee63f8c99ffc44
omid <omidomirzaei@gmail.com>
2022-10-25 14:21:44 +0330

    addded Confirm Message model - usecase

---
+++ lib/app/modules/messages/data/usecases/cofirm_message_usecase.dart
@@ -0,0 +1,48 @@
+import '../../../messaging/controllers/messaging_connection_controller.dart';
+import '../../../messaging/controllers/usecases/send_data_channel_message_usecase.dart';
+import '../models/messages/confirm_message_model.dart';
+import '../models/messages/message_model.dart';
+import '../repo/messages_abstract_repo.dart';
+
+class ConfirmMessage {
+  final MessagesAbstractRepo messagesRepo;
+  MessagingConnectionController messagingConnection;
+  ConfirmMessage({
+    required this.messagesRepo,
+    required this.messagingConnection,
+  });
+  execute({required ConfirmMessageType confirmMessageType}) async {
+    switch (confirmMessageType.runtimeType) {
+      case ConfirmReceivedText:
+        final String messageId = (confirmMessageType as ConfirmReceivedText).messageId;
+
+        Map<String, dynamic> confirmmessageJson = ConfirmMessageModel(
+          messageId: messageId,
+        ).toJson();
+
+        SendDataChannelMessage(messagingConnection: messagingConnection).execute(
+          channelMessageType: ChannelMessageType.confirm(message: confirmmessageJson),
+        );
+        break;
+    }
+  }
+}
+
+class ConfirmMessageType {
+  final String chatId;
+
+  ConfirmMessageType({required this.chatId});
+
+  factory ConfirmMessageType.confirmReceivedText({
+    required String chatId,
+    required String messageId,
+  }) = ConfirmReceivedText;
+}
+
+class ConfirmReceivedText extends ConfirmMessageType {
+  final String messageId;
+  ConfirmReceivedText({
+    required this.messageId,
+    required super.chatId,
+  });
+}
