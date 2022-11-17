a82c3c4d8442b71b86896fc1d2694ad5332cfaa0
omid <omidomirzaei@gmail.com>
2022-11-17 11:57:43 +0330

    fixed dependency injection in ConfirmMessages

--- lib/app/modules/messages/data/usecases/cofirm_message_usecase.dart
+++ lib/app/modules/messages/data/usecases/cofirm_message_usecase.dart
@@ -1,17 +1,21 @@
+import 'package:get/get.dart';
 import '../../../messaging/controllers/messaging_connection_controller.dart';
 import '../../../messaging/usecases/send_data_channel_message_usecase.dart';
 import '../models/messages/confirm_message_model.dart';
-import '../models/messages/message_model.dart';
-import '../repo/messages_abstract_repo.dart';
 
-//TODO messaging: ?
+import '../provider/messages_provider.dart';
+import '../repo/messages_abstract_repo.dart';
+import '../repo/messages_repo.dart';
+
 class ConfirmMessage {
-  final MessagesAbstractRepo messagesRepo;
-  MessagingConnectionController messagingConnection;
-  ConfirmMessage({
-    required this.messagesRepo,
-    required this.messagingConnection,
-  });
+  final MessagesAbstractRepo messagesRepo = MessagesRepo(
+    messagesProvider: MessagesProvider(
+      appDatabaseProvider: Get.find(),
+    ),
+  );
+  final MessagingConnectionController messagingConnection =
+      Get.find<MessagingConnectionController>();
+
   execute({required ConfirmMessageType confirmMessageType}) async {
     switch (confirmMessageType.runtimeType) {
       case ConfirmReceivedText:
