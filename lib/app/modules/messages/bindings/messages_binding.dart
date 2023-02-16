d409d073cba3b245a4985e0a9437da1ac5b916f1
omid <omidomirzaei@gmail.com>
2023-02-16 13:36:29 +0330

    added User Preferences repo bindings

--- lib/app/modules/messages/bindings/messages_binding.dart
+++ lib/app/modules/messages/bindings/messages_binding.dart
@@ -3,19 +3,25 @@ import 'package:heyo/app/modules/messages/data/provider/messages_provider.dart';
 import 'package:heyo/app/modules/messages/data/repo/messages_repo.dart';
 
 import '../../messaging/controllers/messaging_connection_controller.dart';
+import '../../shared/providers/database/dao/user_preferences/user_preferences_provider.dart';
+import '../../shared/providers/database/repos/user_preferences/user_preferences_repo.dart';
 import '../controllers/messages_controller.dart';
 
 class MessagesBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<MessagesController>(
-      () => MessagesController(
+    Get.lazyPut<MessagesController>(() => MessagesController(
           messagesRepo: MessagesRepo(
             messagesProvider: MessagesProvider(
               appDatabaseProvider: Get.find(),
             ),
           ),
-          messagingConnection: Get.find<MessagingConnectionController>()),
-    );
+          messagingConnection: Get.find<MessagingConnectionController>(),
+          userPreferencesRepo: UserPreferencesRepo(
+            userPreferencesProvider: UserPreferencesProvider(
+              appDatabaseProvider: Get.find(),
+            ),
+          ),
+        ));
   }
 }
