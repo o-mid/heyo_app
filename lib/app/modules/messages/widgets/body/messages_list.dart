5636820466cba917d2fa5f8435b573f2a3e3e591
omid <omidomirzaei@gmail.com>
2023-02-21 13:16:37 +0330

    added messages list progress bar

--- lib/app/modules/messages/widgets/body/messages_list.dart
+++ lib/app/modules/messages/widgets/body/messages_list.dart
@@ -29,22 +29,26 @@ class MessagesList extends StatelessWidget {
             controller.isMediaGlassmorphicOpen.value = false;
           }
         },
-        child: ListView.builder(
-            primary: false,
-            controller: controller.scrollController,
-            padding: EdgeInsets.only(top: 54.h, bottom: 16.h),
-            itemCount: controller.messages.length + 1,
-            itemBuilder: (context, index) {
-              if (index == 0) {
-                return BeginningOfMessagesHeader(
-                  chat: controller.args.user.chatModel,
-                );
-              } else {
-                final message = controller.messages[index - 1];
+        child: controller.messages.isEmpty
+            ? const CircularProgressIndicator.adaptive(
+                valueColor: AlwaysStoppedAnimation<Color>(COLORS.kGreenMainColor),
+              )
+            : ListView.builder(
+                primary: false,
+                controller: controller.scrollController,
+                padding: EdgeInsets.only(top: 54.h, bottom: 16.h),
+                itemCount: controller.messages.length + 1,
+                itemBuilder: (context, index) {
+                  if (index == 0) {
+                    return BeginningOfMessagesHeader(
+                      chat: controller.args.user.chatModel,
+                    );
+                  } else {
+                    final message = controller.messages[index - 1];
 
-                return MessageItemWidget(index: index, message: message);
-              }
-            }),
+                    return MessageItemWidget(index: index, message: message);
+                  }
+                }),
       );
     });
   }
