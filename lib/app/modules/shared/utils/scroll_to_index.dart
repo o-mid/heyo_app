6191376e1165b38f1ae38ff5b2fbb82ceeabf545
omid <omidomirzaei@gmail.com>
2023-02-27 12:31:53 +0330

    added Auto Scroll Controller utils

--- lib/app/modules/shared/utils/scroll_to_index.dart
+++ lib/app/modules/shared/utils/scroll_to_index.dart
@@ -630,3 +630,52 @@ Widget buildHighlightTransition(
           .animate(highlight),
       child: child);
 }
+
+/// used to invoke async functions in order
+Future<T> co<T>(key, FutureOr<T> action()) async {
+  for (;;) {
+    final c = _locks[key];
+    if (c == null) break;
+    try {
+      await c.future;
+    } catch (_) {} //ignore error (so it will continue)
+  }
+
+  final c = _locks[key] = new Completer<T>();
+  void then(T result) {
+    final c2 = _locks.remove(key);
+    c.complete(result);
+
+    assert(identical(c, c2));
+  }
+
+  void catchError(ex, StackTrace st) {
+    final c2 = _locks.remove(key);
+    c.completeError(ex, st);
+
+    assert(identical(c, c2));
+  }
+
+  try {
+    final result = action();
+    if (result is Future<T>) {
+      result.then(then).catchError(catchError);
+    } else {
+      then(result);
+    }
+  } catch (ex, st) {
+    catchError(ex, st);
+  }
+
+  return c.future;
+}
+
+final _locks = new HashMap<dynamic, Completer>();
+
+/// skip the TickerCanceled exception
+Future catchAnimationCancel(TickerFuture future) async {
+  return future.orCancel.catchError((_) async {
+    // do nothing, skip TickerCanceled exception
+    return null;
+  }, test: (ex) => ex is TickerCanceled);
+}
