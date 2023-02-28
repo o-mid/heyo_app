0e3a940513bb6fb6d7b384919abe88bc429363c4
omid <omidomirzaei@gmail.com>
2023-02-28 16:56:33 +0330

    changed messages list implementation

--- lib/app/modules/messages/widgets/body/messages_list.dart
+++ lib/app/modules/messages/widgets/body/messages_list.dart
@@ -22,26 +22,43 @@ class MessagesList extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = Get.find<MessagesController>();
 
+    // when the media Media Glassmorphic is Open tap on the rest of the screen will close it
     return Obx(() {
-      // when the media Media Glassmorphic is Open tap on the rest of the screen will close it
       return GestureDetector(
         onTap: () {
+          print(controller.isMediaGlassmorphicOpen);
           if (controller.isMediaGlassmorphicOpen.value) {
             controller.isMediaGlassmorphicOpen.value = false;
           }
         },
-        child: ListView.builder(
-            primary: false,
-            controller: controller.scrollController,
-            padding: EdgeInsets.only(top: 54.h, bottom: 16.h),
-            itemCount: controller.messages.length + 1,
-            itemBuilder: (context, index) {
-              if (index == 0) {
+        child: ListView.custom(
+          primary: false,
+          controller: controller.scrollController,
+          reverse: true,
+          padding: EdgeInsets.only(top: 54.h, bottom: 16.h),
+          // itemCount: controller.messages.length + 1,
+          // itemBuilder: (context, index) {
+          //   if (index == 0) {
+          //     return const BeginningOfMessagesHeaderWidget();
+          //   } else {
+          //     return MessageItemWidget(index: index - 1);
+          //   }
+          // },
+
+          childrenDelegate: SliverChildBuilderDelegate(
+            (context, index) {
+              // reverse index
+              final reverseIndex = controller.messages.length - index;
+              if (reverseIndex == 0) {
                 return const BeginningOfMessagesHeaderWidget();
               } else {
-                return MessageItemWidget(index: index - 1);
+                return MessageItemWidget(
+                    key: ValueKey(controller.messages[reverseIndex - 1].messageId),
+                    index: reverseIndex - 1);
               }
-            }),
+            },
+          ),
+        ),
       );
     });
   }
