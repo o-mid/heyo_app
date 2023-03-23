5db5f0a8942d14e4941d8c48ff7134329653d1f5
omid <omidomirzaei@gmail.com>
2023-03-23 08:59:47 +0330

    removed old-unused shimmer package - changed the implimentation of new package

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -2,15 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:heyo/app/modules/shared/utils/constants/colors.dart';
 import 'package:heyo/generated/assets.gen.dart';
+import 'package:shimmer/shimmer.dart';
 
-import 'package:shimmer_animation/shimmer_animation.dart';
 import '../controllers/splash_controller.dart';
 
 class SplashView extends GetView<SplashController> {
+  const SplashView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Shimmer(
+      body: Shimmer.fromColors(
+        direction: ShimmerDirection.ltr,
+        baseColor: Colors.grey.shade300,
+        highlightColor: Colors.grey.shade100,
         child: Container(
             width: double.infinity,
             height: MediaQuery.of(context).size.height,
