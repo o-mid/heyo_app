4598c77026b6df1059b8d20dcd7cabe6eee8cf39
omid <omidomirzaei@gmail.com>
2023-04-06 15:44:12 +0330

    fixed saved coreid on keychain bug

--- lib/app/modules/p2p_node/p2p_node.dart
+++ lib/app/modules/p2p_node/p2p_node.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter_p2p_communicator/flutter_p2p_communicator.dart';
 import 'package:flutter_p2p_communicator/model/addr_model.dart';
 import 'package:flutter_p2p_communicator/model/req_res_model.dart';
 import 'package:flutter_p2p_communicator/utils/constants.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:heyo/app/modules/p2p_node/data/account/account_info.dart';
 import 'package:heyo/app/modules/p2p_node/p2p_node_request.dart';
 import 'package:heyo/app/modules/p2p_node/p2p_node_response.dart';
@@ -10,6 +13,7 @@ import 'package:heyo/app/modules/p2p_node/p2p_state.dart';
 import 'package:flutter_bip39/bip39.dart';
 import 'package:core_web3dart/src/crypto/formatting.dart';
 import 'package:core_web3dart/web3dart.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class P2PNode {
   final AccountInfo accountInfo;
@@ -32,6 +36,19 @@ class P2PNode {
   }
 
   _startP2PNode() async {
+    // checks if Platform isIOS and if it's the first run of the app clears the Keychain
+    if (Platform.isIOS) {
+      // remove secure storage data on IOS manually,
+      // because it's not cleaned after the app uninstall
+      final prefs = await SharedPreferences.getInstance();
+
+      if (prefs.getBool('first_run') ?? true) {
+        // sets a key-value pair of first run in IOS NSUserDefaults with shared_preferences
+        prefs.setBool('first_run', false);
+        FlutterSecureStorage storage = const FlutterSecureStorage();
+        await storage.deleteAll();
+      }
+    }
     if (await accountInfo.getCoreId() == null) {
       await accountInfo.createAccountAndSaveInStorage();
     }
