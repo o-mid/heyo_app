d7ed5d01a78eb3959f16b8663827053b5fe4cfe4
ali.azimoshan <ali.azimoshan@wallit.eu>
2023-08-25 12:33:24 +0330

    remove inReady and arguments

--- lib/app/modules/add_participate/controllers/add_participate_controller.dart
+++ lib/app/modules/add_participate/controllers/add_participate_controller.dart
@@ -3,12 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:heyo/app/modules/shared/utils/extensions/string.extension.dart';
 
-import '../../new_chat/data/models/new_chat_view_arguments_model.dart';
 import '../../new_chat/data/models/user_model.dart';
 import '../../p2p_node/data/account/account_info.dart';
 import '../../shared/data/repository/contact_repository.dart';
 import '../data/models/filter_model.dart';
-import '../widgets/invite_bttom_sheet.dart';
 
 class AddParticipateController extends GetxController {
   RxList<UserModel> selectedUser = <UserModel>[].obs;
@@ -41,53 +39,12 @@ class AddParticipateController extends GetxController {
   }
 
   @override
-  void onReady() {
-    if (Get.arguments != null) {
-      final args = Get.arguments as NewChatArgumentsModel;
-      // if openInviteBottomSheet is set to true then this will
-      //open the invite bottom sheet right after initializing
-
-      if (args.openInviteBottomSheet) {
-        // TODO: ADD correct profile Invite Links
-        openInviteBottomSheet(profileLink: profileLink);
-      }
-    }
-    super.onReady();
-  }
-
-  @override
   void onClose() {
     inputController.dispose();
     _contactsStreamSubscription.cancel();
   }
 
   final String profileLink = "https://heyo.core/m6ljkB4KJ";
-// Mock data for the users
-  final nearbyUsers = <UserModel>[
-    UserModel(
-      name: "Crapps Wallbanger",
-      walletAddress: 'CB92...969A',
-      coreId: 'CB92...969A',
-      iconUrl:
-          "https://raw.githubusercontent.com/Zunawe/identicons/HEAD/examples/poly.png",
-      nickname: "Nickname",
-    ),
-    UserModel(
-      name: "Fancy Potato",
-      walletAddress: 'CB21...C325',
-      coreId: 'CB21...C325',
-      iconUrl: "https://avatars.githubusercontent.com/u/6634136?v=4",
-      isOnline: true,
-      isVerified: true,
-    ),
-    UserModel(
-      name: "manly Cupholder",
-      walletAddress: 'CB42...324E',
-      coreId: 'CB42...324E',
-      iconUrl: "https://avatars.githubusercontent.com/u/9801359?v=4",
-      isOnline: true,
-    ),
-  ].obs;
 
 // Mock filters for the users
   RxList<FilterModel> filters = [
@@ -117,11 +74,7 @@ class AddParticipateController extends GetxController {
         searchSuggestions.value = newContacts;
       }
     });
-    nearbyUsers.sort(
-      (a, b) => a.name.toLowerCase().compareTo(
-            b.name.toLowerCase(),
-          ),
-    );
+
     super.onInit();
   }
 
@@ -138,7 +91,7 @@ class AddParticipateController extends GetxController {
         searchSuggestions.value = [
           UserModel(
             name: 'unknown',
-            iconUrl: (nearbyUsers..shuffle()).first.iconUrl,
+            iconUrl: profileLink,
             walletAddress: query,
             coreId: query,
           )
