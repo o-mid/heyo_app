04ca73c1203b90802d9bdf04e2e19ab227245e79
ali.azimoshan <ali.azimoshan@wallit.eu>
2023-09-06 22:18:58 +0330

    finish the ui bugs | slidable and sizing issue

--- lib/app/modules/calls/home/widgets/call_log_widget.dart
+++ lib/app/modules/calls/home/widgets/call_log_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+
 import 'package:heyo/app/modules/calls/home/controllers/calls_controller.dart';
 import 'package:heyo/app/modules/calls/shared/data/models/call_model.dart';
 import 'package:heyo/app/modules/calls/shared/widgets/call_status_icon_and_date.dart';
@@ -9,10 +10,9 @@ import 'package:heyo/app/modules/shared/utils/constants/colors.dart';
 import 'package:heyo/app/modules/shared/utils/constants/textStyles.dart';
 import 'package:heyo/app/modules/shared/utils/screen-utils/sizing/custom_sizes.dart';
 import 'package:heyo/app/modules/shared/widgets/curtom_circle_avatar.dart';
+import 'package:heyo/app/modules/shared/widgets/slidable_widget.dart';
 import 'package:heyo/app/routes/app_pages.dart';
 import 'package:heyo/generated/assets.gen.dart';
-
-import 'package:swipeable_tile/swipeable_tile.dart';
 
 class CallLogWidget extends StatelessWidget {
   final CallModel call;
@@ -24,7 +24,7 @@ class CallLogWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.find<CallsController>();
-    return GestureDetector(
+    return InkWell(
       onTap: () {
         Get.toNamed(
           Routes.USER_CALL_HISTORY,
@@ -34,28 +34,10 @@ class CallLogWidget extends StatelessWidget {
           ),
         );
       },
-      child: SwipeableTile.swipeToTrigger(
+      child: SlidableWidget(
         key: Key(call.id),
-        isElevated: false,
-        swipeThreshold: 0.21,
-        borderRadius: 0,
-        onSwiped: (SwipeDirection direction) {
-          controller.showDeleteCallDialog(call);
-        },
-        color: COLORS.kAppBackground,
-        backgroundBuilder:
-            (BuildContext context, SwipeDirection direction, AnimationController progress) {
-          return Container(
-            alignment: Alignment.centerRight,
-            color: COLORS.kStatesErrorColor,
-            padding: EdgeInsets.symmetric(horizontal: 30.w),
-            child: Assets.svg.deleteIcon.svg(
-              color: COLORS.kWhiteColor,
-              width: 18.w,
-              height: 18.w,
-            ),
-          );
-        },
+        onDismissed: () => controller.deleteCall(call),
+        confirmDismiss: () => controller.showDeleteCallDialog(call),
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 8.h),
           child: Row(
