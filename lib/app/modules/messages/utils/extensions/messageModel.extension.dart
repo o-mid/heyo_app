22476230d5e0a6915502340850db22e0a3de3b05
omid <omidomirzaei@gmail.com>
2023-09-19 12:55:47 +0330

    fixed reply to messages preview

--- lib/app/modules/messages/utils/extensions/messageModel.extension.dart
+++ lib/app/modules/messages/utils/extensions/messageModel.extension.dart
@@ -1,9 +1,13 @@
 import 'package:get/get.dart';
+import 'package:heyo/app/modules/messages/data/models/messages/call_message_model.dart';
+import 'package:heyo/app/modules/messages/data/models/messages/file_message_model.dart';
 import 'package:heyo/app/modules/messages/data/models/messages/location_message_model.dart';
+import 'package:heyo/app/modules/messages/data/models/messages/message_model.dart';
 
 import '../../../../../generated/locales.g.dart';
 import '../../data/models/messages/audio_message_model.dart';
 import '../../data/models/messages/image_message_model.dart';
+import '../../data/models/messages/message_model.dart';
 import '../../data/models/messages/message_model.dart';
 import '../../data/models/messages/multi_media_message_model.dart';
 import '../../data/models/messages/text_message_model.dart';
@@ -12,27 +16,34 @@ import '../../data/models/messages/video_message_model.dart';
 extension MessageModelHelpers on MessageModel {
   String getReplyMsgText() {
     String replyMsg = "";
-    switch (runtimeType) {
-      case TextMessageModel:
-        replyMsg = (MessageModel as TextMessageModel).text;
-        break;
-      case ImageMessageModel:
-        replyMsg = LocaleKeys.MessagesPage_replyToImage.tr;
-        break;
-      case VideoMessageModel:
-        // Todo: use video metadata to show video duration
-        replyMsg = LocaleKeys.MessagesPage_replyToVideo.tr;
-        break;
-      case AudioMessageModel:
-        // Todo: use video metadata to show audio duration
-        replyMsg = LocaleKeys.MessagesPage_replyToAudio.tr;
-        break;
-      case MultiMediaMessageModel:
-        replyMsg = LocaleKeys.MessagesPage_replyToMultiMedia.tr;
+    switch (type) {
+      case MessageContentType.text:
+        replyMsg = (this as TextMessageModel).text;
         break;
 
-      case LocationMessageModel:
+      case MessageContentType.image:
+        replyMsg = LocaleKeys.MessagesPage_replyToImage.tr;
+        break;
+      case MessageContentType.video:
+        replyMsg = LocaleKeys.MessagesPage_replyToVideo.tr;
+        break;
+      case MessageContentType.audio:
+        replyMsg = LocaleKeys.MessagesPage_replyToAudio.tr;
+        break;
+      case MessageContentType.file:
+        replyMsg = (this as FileMessageModel).metadata.name;
+        break;
+      case MessageContentType.call:
+        replyMsg = "${(this as CallMessageModel).callType.name} Call";
+        break;
+      case MessageContentType.location:
         replyMsg = LocaleKeys.MessagesPage_replyToLocation.tr;
+        break;
+      case MessageContentType.liveLocation:
+        replyMsg = LocaleKeys.MessagesPage_replyToLocation.tr;
+        break;
+      case MessageContentType.multiMedia:
+        replyMsg = LocaleKeys.MessagesPage_replyToMultiMedia.tr;
         break;
     }
 
