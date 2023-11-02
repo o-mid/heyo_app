5d87ef511315c5bab60a2ee6047018bec9762a0c
Hosein Haqiqian <hosein.haqiqian@wallit.eu>
2023-11-02 17:20:53 +0330

    added logout button

--- lib/app/modules/account/controllers/account_controller.dart
+++ lib/app/modules/account/controllers/account_controller.dart
@@ -1,16 +1,23 @@
 import 'package:get/get.dart';
 import 'package:heyo/app/modules/shared/data/repository/crypto_account/crypto_account_repo.dart';
+import 'package:heyo/app/routes/app_pages.dart';
 
 class AccountController extends GetxController {
-  final CryptoAccountRepository accountInfoRepo;
+  final AccountRepository _accountInfoRepo;
   final coreId = "".obs;
 
-  AccountController({required this.accountInfoRepo});
+  AccountController({required AccountRepository accountInfoRepo})
+      : _accountInfoRepo = accountInfoRepo;
 
   @override
   void onInit() async {
     super.onInit();
-    coreId.value = (await accountInfoRepo.getUserContactAddress()) ?? "";
+    coreId.value = (await _accountInfoRepo.getUserAddress()) ?? "";
+  }
+
+  Future<void> logout() async {
+    await _accountInfoRepo.logout();
+    await Get.offAllNamed(Routes.INTRO);
   }
 
   @override
