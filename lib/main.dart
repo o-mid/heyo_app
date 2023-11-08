d8329d5851ecfb7d30a976129c06e878b98f6a6c
omid <omidomirzaei@gmail.com>
2023-11-08 14:55:46 +0330

    added Sentry initialization - DNS servers

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -6,17 +8,33 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:heyo/app/modules/shared/bindings/global_bindings.dart';
 import 'package:heyo/app/routes/app_pages.dart';
 import 'package:heyo/generated/locales.g.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+import 'app/modules/shared/utils/constants/strings_constant.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = Platform.isAndroid ? SENTRY_ANDROID_DNS : SENTRY_IOS_DNS;
+    },
+    // Init your App.
+    appRunner: () => initApp(),
+  );
+}
 
+void initApp() {
   runApp(
     ScreenUtilInit(
       //  Width and height from figma design
       designSize: const Size(375, 712),
 
       builder: (_, child) => GetMaterialApp(
+        navigatorObservers: [
+          SentryNavigatorObserver(),
+        ],
+
         /// about ```translationsKeys``` see the Getx package on internationalization https://pub.dev/packages/get#internationalization
         // translationsKeys: AppTranslation.translations,
         title: 'Heyo',
