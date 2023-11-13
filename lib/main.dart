dee83f00a8e04cc26382fcba064e0f9319fde34b
Hosein Haqiqian <hosein.haqiqian@wallit.eu>
2023-11-13 17:35:26 +0330

    remove log

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
