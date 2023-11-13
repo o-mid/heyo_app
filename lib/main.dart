9f1a6aa941a5a6cc3304c1f81efbd19d3ab32bf2
Hosein Haqiqian <hosein.haqiqian@wallit.eu>
2023-11-13 17:35:26 +0330

    remove log

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -9,17 +9,16 @@ import 'package:heyo/app/modules/shared/bindings/global_bindings.dart';
 import 'package:heyo/app/routes/app_pages.dart';
 import 'package:heyo/generated/locales.g.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
+
 import 'app/modules/shared/utils/constants/strings_constant.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   // only activate sentry in release mode
   if (kReleaseMode) {
     await SentryFlutter.init(
