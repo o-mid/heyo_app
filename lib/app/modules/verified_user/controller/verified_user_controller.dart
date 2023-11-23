1af3043760b554b78913b26e35365b733546cbf1
Hosein Haqiqian <hosein.haqiqian@wallit.eu>
2023-11-23 13:23:52 +0330

    removed unnecessary code

--- lib/app/modules/verified_user/controller/verified_user_controller.dart
+++ lib/app/modules/verified_user/controller/verified_user_controller.dart
@@ -3,9 +3,7 @@ import 'package:heyo/app/modules/connection/domain/connection_contractor.dart';
 import 'package:heyo/app/routes/app_pages.dart';
 
 class VerifiedUserController extends GetxController {
-  VerifiedUserController({required this.libP2PConnectionContractor});
-
-  ConnectionContractor libP2PConnectionContractor;
+  VerifiedUserController();
 
   @override
   void onInit() {
