ee9bbf4585820315130b89b7cbd66f9567ca95b0
Hosein Haqiqian <hosein.haqiqian@wallit.eu>
2023-11-27 15:15:08 +0330

    resolve conflicts with develop

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
