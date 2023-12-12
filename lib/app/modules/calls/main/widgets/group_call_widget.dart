3075782932e7759041b1d883dfb93a53e121ae12
ali.azimoshan <ali.azimoshan@wallit.eu>
2023-12-12 17:17:57 +0330

    update group call widget

--- lib/app/modules/calls/main/widgets/group_call_widget.dart
+++ lib/app/modules/calls/main/widgets/group_call_widget.dart
@@ -3,16 +3,24 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'package:heyo/app/modules/calls/main/controllers/call_controller.dart';
+import 'package:heyo/app/modules/calls/main/widgets/call_renderer_local_stack_widget.dart';
 import 'package:heyo/app/modules/calls/main/widgets/call_renderer_widget.dart';
+import 'package:heyo/app/modules/calls/main/widgets/draggable_video.dart';
+import 'package:heyo/app/modules/shared/utils/constants/transitions_constant.dart';
 
 class GroupCallWidget extends GetView<CallController> {
   const GroupCallWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final allVideoRenderers = controller.getAllConnectedParticipate();
+    //final allVideoRenderers = controller.getAllConnectedParticipate();
+    final localParticipate = controller.localParticipate;
+    final remoteParticipate = controller.connectedRemoteParticipates;
     return Obx(() {
-      if (allVideoRenderers.isEmpty) {
+      final changeCallerWidgetSize = controller.showCallerOptions.value &
+          controller.isVideoPositionsFlipped.isFalse;
+
+      if (remoteParticipate.isEmpty || localParticipate.value == null) {
         return const Center(child: Text('No remote videos available.'));
       }
 
@@ -22,16 +30,40 @@ class GroupCallWidget extends GetView<CallController> {
             child: GridView.builder(
               shrinkWrap: true,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: allVideoRenderers.length == 2 ? 1 : 2,
+                crossAxisCount: remoteParticipate.length == 2 ? 1 : 2,
                 childAspectRatio: ScreenUtil.defaultSize.aspectRatio *
-                    (allVideoRenderers.length == 2 ? 2 : 1),
+                    (remoteParticipate.length == 2 ? 2 : 1),
               ),
-              itemCount: allVideoRenderers.length,
+              itemCount: remoteParticipate.length,
               itemBuilder: (context, index) {
                 return CallRendererWidget(
-                  participantModel: allVideoRenderers[index],
+                  participantModel: remoteParticipate[index],
                 );
               },
+            ),
+          ),
+          DraggableVideo(
+            child: AnimatedSize(
+              clipBehavior: Clip.hardEdge,
+              curve: TRANSITIONS.callPage_DraggableVideoAnimatedSizeCurve,
+              alignment: Alignment.bottomLeft,
+              duration: controller.callerScaleDuration,
+              reverseDuration: controller.callerScaleReverseDuration,
+              child: SizedBox(
+                width: changeCallerWidgetSize ? 200.w : 120.w,
+                height: changeCallerWidgetSize ? 250.h : 170.h,
+                child: GestureDetector(
+                  //TODO: AliAzim => check if we need this onTap & onDoubleTap or not
+                  onTap: controller.changeCallerOptions,
+                  onDoubleTap: controller.flipVideoPositions,
+                  child: Container(
+                    margin: const EdgeInsets.all(5),
+                    child: CallRendererLocalStackWidget(
+                      participantModel: localParticipate.value!,
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
           //* This sizeBox is because of call bottom sheet header
