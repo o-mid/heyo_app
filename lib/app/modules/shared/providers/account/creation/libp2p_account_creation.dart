c8bc30d0b19d5425cc69de22c4beed1b0c3be409
Hosein Haqiqian <hosein.haqiqian@wallit.eu>
2023-12-13 17:14:26 +0330

    fixed sending object through isolate

--- lib/app/modules/shared/providers/account/creation/libp2p_account_creation.dart
+++ lib/app/modules/shared/providers/account/creation/libp2p_account_creation.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/foundation.dart';
+import 'package:get/get.dart';
 import 'package:heyo/app/modules/p2p_node/data/key/cryptography_key_generator.dart';
+import 'package:heyo/app/modules/p2p_node/data/key/web3_keys.dart';
+import 'package:heyo/app/modules/shared/bindings/global_bindings.dart';
 import 'package:heyo/app/modules/shared/data/models/create_account_result.dart';
 import 'package:heyo/app/modules/shared/data/providers/secure_storage/local_storages_abstract.dart';
 import 'package:heyo/app/modules/shared/providers/account/creation/account_creation.dart';
 import 'package:heyo/app/modules/shared/providers/crypto/storage/libp2p_storage_provider.dart';
+import 'dart:isolate';
 
 class LibP2PAccountCreation extends AccountCreation {
   final LocalStorageAbstractProvider localProvider;
@@ -18,8 +22,7 @@ class LibP2PAccountCreation extends AccountCreation {
 
   @override
   Future<CreateAccountResult> createAccount() async {
-    final result = compute((message) => _createAccount(), null);
-    return result;
+    return compute((_) => _createAccount(), null);
   }
 
   @override
@@ -33,25 +36,31 @@ class LibP2PAccountCreation extends AccountCreation {
       accountResult.pubKey,
     );
   }
+}
 
-  Future<CreateAccountResult> _createAccount() async {
-    // generate the mnemonic from the cryptographyKeyGenerator
-    final phrases = cryptographyKeyGenerator.generate_mnemonic();
-    // generate the private key from the cryptographyKeyGenerator using the mnemonic and a password
-    final privateKey = await cryptographyKeyGenerator
-        .generatePrivateKeysFromMneomonic(phrases, "198491");
+Future<CreateAccountResult> _createAccount() async {
+  // generate the mnemonic from the cryptographyKeyGenerator
 
-    final address = await cryptographyKeyGenerator.generateAddress(privateKey);
-    final pubKey =
-        await cryptographyKeyGenerator.getPublicKeyFromPrivate(privateKey);
-    final coreId =
-        await cryptographyKeyGenerator.generateCoreIdFromPriv(privateKey);
-    return CreateAccountResult(
-      address: address,
-      coreId: coreId,
-      phrases: phrases,
-      privateKey: privateKey,
-      pubKey: pubKey,
-    );
-  }
+  final cryptographyKeyGenerator =
+      Web3Keys(web3client: GlobalBindings.web3Client);
+
+  final phrases = cryptographyKeyGenerator.generate_mnemonic();
+
+// generate the private key from the cryptographyKeyGenerator using the mnemonic and a password
+  final privateKey = await cryptographyKeyGenerator
+      .generatePrivateKeysFromMneomonic(phrases, "198491");
+
+  final address = await cryptographyKeyGenerator.generateAddress(privateKey);
+  final pubKey =
+      await cryptographyKeyGenerator.getPublicKeyFromPrivate(privateKey);
+  final coreId =
+      await cryptographyKeyGenerator.generateCoreIdFromPriv(privateKey);
+
+  return CreateAccountResult(
+    coreId: coreId,
+    privateKey: privateKey,
+    phrases: phrases,
+    address: address,
+    pubKey: pubKey,
+  );
 }
