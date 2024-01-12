b87a321c95b1b44b7b1667fc2cb1488c625c22b3
farzam <farzam.aghamohammadi@wallit.eu>
2024-01-12 16:40:07 +0300

    Fix notification on release mode

--- lib/app/modules/calls/data/call_background_request.dart
+++ lib/app/modules/calls/data/call_background_request.dart
@@ -4,6 +4,17 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:heyo/app/modules/calls/data/notification_processor.dart';
 import 'package:heyo/main.dart';
 
+
+@pragma('vm:entry-point')
+Future<void> onBackgroundMessage(RemoteMessage message) async {
+  NotificationProcessor.process(
+    message.sentTime,
+    jsonEncode(message.data),
+    flutterLocalNotificationsPlugin: flutterLocalNotificationsPlugin,
+    isBackgroundNotification: true,
+  );
+}
+
 Future<void> onMessageReceived(
   RemoteMessage message, {
   required bool isFromBackground,
@@ -17,11 +28,3 @@ Future<void> onMessageReceived(
   );
 }
 
-Future<void> onBackgroundMessage(RemoteMessage message) async {
-  NotificationProcessor.process(
-    message.sentTime,
-    jsonEncode(message.data),
-    flutterLocalNotificationsPlugin: flutterLocalNotificationsPlugin,
-    isBackgroundNotification: true,
-  );
-}
