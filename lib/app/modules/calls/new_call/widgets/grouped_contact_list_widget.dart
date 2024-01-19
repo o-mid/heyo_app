c8c577c818ae7f12bb39604077eeec58394e63d8
ali.azimoshan <ali.azimoshan@wallit.eu>
2024-01-19 18:04:33 +0330

    add animation for new call page

--- lib/app/modules/calls/new_call/widgets/grouped_contact_list_widget.dart
+++ lib/app/modules/calls/new_call/widgets/grouped_contact_list_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 import 'package:heyo/app/modules/calls/new_call/controllers/new_call_controller.dart';
 import 'package:heyo/app/modules/new_chat/widgets/user_widget.dart';
+import 'package:heyo/app/modules/shared/widgets/animate_list_widget.dart';
 import 'package:heyo/app/modules/shared/widgets/list_header_widget.dart';
 
 class GroupedContactListWidget extends GetView<NewCallController> {
@@ -10,18 +12,15 @@ class GroupedContactListWidget extends GetView<NewCallController> {
   @override
   Widget build(BuildContext context) {
     return Obx(() {
-      return ListView.builder(
-        shrinkWrap: true,
-        itemCount: controller.groupedContact.length,
-        itemBuilder: (context, index) {
+      return AnimateListWidget(
+        children: controller.groupedContact.keys.map((key) {
           return Obx(() {
-            final firstChar = controller.groupedContact.keys.elementAt(index);
-            final contactsForChar = controller.groupedContact[firstChar]!;
+            final contactsForChar = controller.groupedContact[key]!;
 
             return Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ListHeaderWidget(title: firstChar),
+                ListHeaderWidget(title: key),
                 Column(
                   children: contactsForChar
                       .map(
@@ -41,8 +40,16 @@ class GroupedContactListWidget extends GetView<NewCallController> {
               ],
             );
           });
-        },
+        }).toList(),
       );
+
+      //return ListView.builder(
+      //  shrinkWrap: true,
+      //  itemCount: controller.groupedContact.length,
+      //  itemBuilder: (context, index) {
+      //    return
+      //  },
+      //);
     });
   }
 }
