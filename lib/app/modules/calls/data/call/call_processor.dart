69424541c41c69e5afa39d60ae3e8ed3ac6c2f0d
Hosein Haqiqian <hosein.haqiqian@wallit.eu>
2024-01-20 18:06:35 -0500

    implement call missed call handling and prevent call notification duplication

--- lib/app/modules/calls/data/call/call_processor.dart
+++ lib/app/modules/calls/data/call/call_processor.dart
@@ -4,17 +4,19 @@ import 'package:get/get.dart';
 import 'package:heyo/app/modules/calls/data/call_requests_processor.dart';
 import 'package:heyo/app/modules/calls/data/model/notification_call_model.dart';
 import 'package:heyo/app/modules/shared/utils/extensions/map.extension.dart';
+import 'package:heyo/app/routes/app_pages.dart';
 
 class CallProcessor {
   static int callNotificationId = 10;
 
-  void setup({
-    required DateTime? messageCreationTime,
-    required Map<String, dynamic> data,
-  }) {
-    if (messageCreationTime != null) {
-      _addNotificationCreationTimeToPayload(data, messageCreationTime);
+  bool isExpired(DateTime? messageCreated) {
+    if (messageCreated != null) {
+      final diff = DateTime.now().millisecondsSinceEpoch -
+          messageCreated!.millisecondsSinceEpoch;
+      // Skip if more than 20 seconds
+      if (diff > 20 * 1000) return true;
     }
+    return false;
   }
 
   void process({
@@ -22,45 +24,43 @@ class CallProcessor {
     required Map<String, dynamic> data,
     required FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin,
   }) {
-    final messageTime = DateTime.fromMillisecondsSinceEpoch(
-      int.parse(data['dateTime'].toString()),
-    );
-    final diff = DateTime.now().millisecondsSinceEpoch -
-        messageTime.millisecondsSinceEpoch;
-    // Skip if more than 20 seconds
-    if (diff > 20 * 1000) return;
-    if (isBackgroundNotification) {
-      _showNotificationWithActions(
-        flutterLocalNotificationPlugin: flutterLocalNotificationsPlugin,
-        channelName: 'Calls',
-        title: 'Heyo',
-        body: 'Incoming Call from Heyo',
-        actions: [
-          AndroidNotificationAction('1', 'Accept', showsUserInterface: true),
-          AndroidNotificationAction('2', 'Deny')
-        ],
-        payload: jsonEncode(data),
-      );
-    } else {
-      _openIncomingCallPage(jsonEncode(data), flutterLocalNotificationsPlugin);
+    final callData = NotificationCallModel.fromJson(data);
+
+    if ([Routes.INCOMING_CALL, Routes.CALL].contains(Get.currentRoute)) {
+      /// in future, we have to show a missed call if caller is different from
+      /// current one
+      return;
     }
+    if (isExpired(callData.content!.dateTime)) {
+      _showMissedCall(callData, flutterLocalNotificationsPlugin);
+      return;
+    }
+
+    if (isBackgroundNotification) {
+      _showIncomingCallNotification(flutterLocalNotificationsPlugin, callData);
+      return;
+    }
+
+    _openIncomingCallPage(callData, flutterLocalNotificationsPlugin);
   }
 
-  Future<void> _showNotificationWithActions({
+  Future<void> _showNotification({
     required String channelName,
     required String title,
     required String body,
     required String payload,
     required List<AndroidNotificationAction> actions,
     required FlutterLocalNotificationsPlugin flutterLocalNotificationPlugin,
+    required bool fullScreen,
+    bool important = false,
   }) async {
     final androidNotificationDetails = AndroidNotificationDetails(
       '0',
       channelName,
-      fullScreenIntent: true,
+      fullScreenIntent: fullScreen,
       actions: actions,
-      priority: Priority.high,
-      importance: Importance.high,
+      priority: important ? Priority.max : Priority.defaultPriority,
+      importance: important ? Importance.max : Importance.defaultImportance,
       timeoutAfter: 20 * 1000,
     );
     final notificationDetails =
@@ -74,12 +74,8 @@ class CallProcessor {
     );
   }
 
-  Future<void> _openIncomingCallPage(String payload,
+  Future<void> _openIncomingCallPage(NotificationCallModel notificationContent,
       FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
-    final notificationContent =
-        NotificationCallModel.fromJson(jsonDecode(payload));
-
-
     // cancel notification
     await flutterLocalNotificationsPlugin.cancel(callNotificationId);
 
@@ -92,13 +88,36 @@ class CallProcessor {
     );
   }
 
-  void _addNotificationCreationTimeToPayload(
-      Map<String, dynamic> data, DateTime messageSent) {
-    if (data['dateTime'] == null) {
-      data.addToMap(
-        'dateTime',
-        messageSent.microsecondsSinceEpoch.toString(),
-      );
-    }
+
+  void _showMissedCall(NotificationCallModel data,
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) {
+    _showNotification(
+      channelName: 'MissedCalls',
+      title: 'Missed Call',
+      fullScreen: false,
+      body: 'Missed call from ${data.messageFrom ?? ''}',
+      payload: jsonEncode(data.toJson()),
+      actions: [],
+      flutterLocalNotificationPlugin: flutterLocalNotificationsPlugin,
+    );
+  }
+
+  void _showIncomingCallNotification(
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin,
+    NotificationCallModel data,
+  ) {
+    _showNotification(
+      flutterLocalNotificationPlugin: flutterLocalNotificationsPlugin,
+      channelName: 'Calls',
+      title: 'Heyo',
+      body: 'Incoming Call from Heyo',
+      fullScreen: true,
+      important: true,
+      actions: [
+        AndroidNotificationAction('1', 'Accept', showsUserInterface: true),
+        AndroidNotificationAction('2', 'Deny')
+      ],
+      payload: jsonEncode(data.toJson()),
+    );
   }
 }
