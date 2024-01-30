1189ddf86c230a15b7a6c78b4f703ae75742f2cb
ali.azimoshan <ali.azimoshan@wallit.eu>
2024-01-30 20:15:53 +0330

    removed unUsed import

--- lib/app/modules/calls/call_history_detail/widgets/call_history_multi_participant_widget.dart
+++ lib/app/modules/calls/call_history_detail/widgets/call_history_multi_participant_widget.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+
 import 'package:heyo/app/modules/calls/call_history_detail/controllers/call_history_detail_controller.dart';
-import 'package:heyo/app/modules/calls/call_history_detail/widgets/call_history_detail_avatar_widget.dart';
 import 'package:heyo/app/modules/calls/call_history_detail/widgets/call_history_detail_list_tile_widget.dart';
 import 'package:heyo/app/modules/calls/call_history_detail/widgets/multi_participant_header_widget.dart';
-import 'package:heyo/app/modules/shared/data/models/call_view_arguments_model.dart';
 import 'package:heyo/app/modules/shared/utils/constants/colors.dart';
 import 'package:heyo/app/modules/shared/utils/constants/textStyles.dart';
-import 'package:heyo/app/modules/shared/utils/extensions/core_id.extension.dart';
 import 'package:heyo/app/modules/shared/utils/extensions/datetime.extension.dart';
 import 'package:heyo/app/modules/shared/utils/screen-utils/sizing/custom_sizes.dart';
 import 'package:heyo/app/modules/shared/widgets/animate_list_widget.dart';
-import 'package:heyo/app/modules/shared/widgets/appbar_widget.dart';
-import 'package:heyo/app/modules/shared/widgets/circle_icon_button.dart';
-import 'package:heyo/app/routes/app_pages.dart';
-import 'package:heyo/generated/assets.gen.dart';
 import 'package:heyo/generated/locales.g.dart';
 
 class CallHistoryMultiParticipantWidget
