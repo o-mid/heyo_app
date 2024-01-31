c65a0fc5b5383f1092789d2bcbf27b924389f15d
ali.azimoshan <ali.azimoshan@wallit.eu>
2024-01-31 10:54:30 +0330

    fix the issue of wrong title and button title in the add contact page

--- lib/app/modules/add_contacts/views/add_contacts_view.dart
+++ lib/app/modules/add_contacts/views/add_contacts_view.dart
@@ -15,123 +15,125 @@ import 'package:heyo/generated/assets.gen.dart';
 import 'package:heyo/generated/locales.g.dart';
 
 class AddContactsView extends GetView<AddContactsController> {
-  const AddContactsView({Key? key}) : super(key: key);
+  const AddContactsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        backgroundColor: COLORS.kGreenMainColor,
-        elevation: 0,
-        centerTitle: false,
-        title: controller.isContact.value ? Text(LocaleKeys.AddContacts_addToContacts.tr,
-          style: const TextStyle(
-            fontWeight: FONTS.Bold,
-            fontFamily: FONTS.interFamily,
+    return Obx(
+      () {
+        return Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            backgroundColor: COLORS.kGreenMainColor,
+            elevation: 0,
+            centerTitle: false,
+            title: Text(
+              controller.isContact.value
+                  ? LocaleKeys.AddContacts_addToContacts.tr
+                  : LocaleKeys.AddContacts_Edit_Contact.tr,
+              style: const TextStyle(
+                fontWeight: FONTS.Bold,
+                fontFamily: FONTS.interFamily,
+              ),
+            ),
           ),
-        ) : Text(LocaleKeys.AddContacts_Edit_Contact.tr,
-          style: const TextStyle(
-            fontWeight: FONTS.Bold,
-            fontFamily: FONTS.interFamily,
-          ),
-        ),
-        automaticallyImplyLeading: true,
-      ),
-      body: Obx(() {
-        return Padding(
-          padding: CustomSizes.mainContentPadding,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  CustomSizes.largeSizedBoxHeight,
-                  CustomCircleAvatar(
-                    coreId: controller.args.coreId,
-                    size: 64,
-                  ),
-                  CustomSizes.mediumSizedBoxHeight,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        controller.nickname.value.isEmpty
-                            ? controller.args.coreId.shortenCoreId
-                            : controller.nickname.value,
-                        style: TEXTSTYLES.kHeaderLarge.copyWith(color: COLORS.kDarkBlueColor),
+          body: Padding(
+            padding: CustomSizes.mainContentPadding,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    CustomSizes.largeSizedBoxHeight,
+                    CustomCircleAvatar(
+                      coreId: controller.args.coreId,
+                      size: 64,
+                    ),
+                    CustomSizes.mediumSizedBoxHeight,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          controller.nickname.value.isEmpty
+                              ? controller.args.coreId.shortenCoreId
+                              : controller.nickname.value,
+                          style: TEXTSTYLES.kHeaderLarge
+                              .copyWith(color: COLORS.kDarkBlueColor),
+                        ),
+                        CustomSizes.smallSizedBoxWidth,
+                        if (controller.isVerified.value)
+                          Assets.svg.verifiedWithBluePadding.svg(
+                            height: 24.w,
+                            width: 24.w,
+                          )
+                        else
+                          const SizedBox(),
+                      ],
+                    ),
+                    CustomSizes.smallSizedBoxHeight,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          controller.args.coreId.shortenCoreId,
+                          style: TEXTSTYLES.kBodySmall.copyWith(
+                            color: COLORS.kTextBlueColor,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            CustomSizes.smallSizedBoxWidth,
+                            Assets.svg.dotIndicator.svg(),
+                            CustomSizes.smallSizedBoxWidth,
+                          ],
+                        )
+                      ],
+                    ),
+                    CustomSizes.smallSizedBoxHeight,
+                    SizedBox(
+                      height: 40.h,
+                    ),
+                    CustomTextField(
+                      labelText: LocaleKeys.AddContacts_addNickname.tr,
+                      onChanged: (String value) =>
+                          controller.setNickname(value),
+                      textController: controller.myNicknameController,
+                    ),
+                    CustomSizes.mediumSizedBoxHeight,
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        LocaleKeys.AddContacts_AddNicknameSubtitle.tr,
+                        style: TEXTSTYLES.kBodySmall,
                       ),
-                      CustomSizes.smallSizedBoxWidth,
-                      controller.isVerified.value
-                          ? Assets.svg.verifiedWithBluePadding.svg(
-                              alignment: Alignment.center,
-                              height: 24.w,
-                              width: 24.w,
-                            )
-                          : const SizedBox(),
-                    ],
-                  ),
-                  CustomSizes.smallSizedBoxHeight,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        controller.args.coreId.shortenCoreId,
-                        style: TEXTSTYLES.kBodySmall.copyWith(
-                          color: COLORS.kTextBlueColor,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    CustomButton.primary(
+                      // TODO : chatModel should be filled with correct data
+                      onTap: () async {
+                        await controller.updateContact();
+                      },
+
+                      titleWidget: Text(
+                        controller.isContact.value
+                            ? LocaleKeys.AddContacts_buttons_addToContacts.tr
+                            : LocaleKeys.AddContacts_Update_Contact.tr,
+                        style: TEXTSTYLES.kLinkBig.copyWith(
+                          color: COLORS.kWhiteColor,
                         ),
                       ),
-                      Row(
-                        children: [
-                          CustomSizes.smallSizedBoxWidth,
-                          Assets.svg.dotIndicator.svg(),
-                          CustomSizes.smallSizedBoxWidth,
-                        ],
-                      )
-                    ],
-                  ),
-                  CustomSizes.smallSizedBoxHeight,
-                  SizedBox(
-                    height: 40.h,
-                  ),
-                  CustomTextField(
-                    labelText: LocaleKeys.AddContacts_addNickname.tr,
-                    onChanged: (String value) => controller.setNickname(value),
-                    textController: controller.myNicknameController,
-                  ),
-                  CustomSizes.mediumSizedBoxHeight,
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      LocaleKeys.AddContacts_AddNicknameSubtitle.tr,
-                      style: TEXTSTYLES.kBodySmall,
                     ),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  CustomButton.primary(
-                    // TODO : chatModel should be filled with correct data
-                    onTap: () async {
-                      await controller.updateContact();
-                    },
-
-                    titleWidget: Text(
-                      controller.isContact.value ? LocaleKeys.AddContacts_Update_Contact.tr : LocaleKeys.AddContacts_buttons_addToContacts.tr,
-                      style: TEXTSTYLES.kLinkBig.copyWith(
-                        color: COLORS.kWhiteColor,
-                      ),
-                    ),
-                  ),
-                  CustomSizes.largeSizedBoxHeight,
-                ],
-              )
-            ],
+                    CustomSizes.largeSizedBoxHeight,
+                  ],
+                ),
+              ],
+            ),
           ),
         );
-      }),
+      },
     );
   }
 }
