9db28b67b1b5e5e52b8be64d4f0386ab5fa263be
ali.azimoshan <ali.azimoshan@wallit.eu>
2024-03-15 15:03:48 +0330

    add go router configuration and page to Heyo

---
+++ lib/core/navigation/route_paths.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: constant_identifier_names
+part of 'go_router.dart';
+
+abstract class RoutePaths {
+  static const SPLASH = '/splash';
+  static const INTRO = '/intro';
+  static const SING_UP = '/sing-up';
+  static const PIN_CODE = '/pin-code';
+  static const GENERATE_PRIVATE_KEYS = '/generate-private-keys';
+  static const MAIN_SCREEN = '/main-screen';
+  static const CHATS = '/chats';
+  static const CALLS = '/calls';
+  static const WALLET = '/wallet';
+  static const SEARCH_NEARBY = '/search-nearby';
+  static const ACCOUNT = '/account';
+  static const HOME = '/home';
+  static const NEW_CHAT = '/new-chat';
+  static const MESSAGES = '/messages';
+  static const ADD_CONTACTS = '/add-contacts';
+  static const FORWARD_MASSAGES = '/forward-massages';
+  static const GALLERY_PICKER = '/gallery-picker';
+  static const SHARE_LOCATION = '/share-location';
+  static const SHAREABLE_QR = '/shareable-qr';
+  static const SCAN_QR = '/scan-qr';
+  static const USER_CALL_HISTORY = '/user-call-history';
+  static const CALL = '/call';
+  static const INCOMING_CALL = '/incoming-call';
+  static const SHARE_FILES = '/share-files';
+  static const MEDIA_VIEW = '/media-view';
+  static const CONTACTS = '/contacts';
+  static const NEW_CALL = '/new-call';
+  static const WIFI_DIRECT = '/wifi-direct';
+  static const WIFI_DIRECT_CONNECT = '/wifi-direct-connect';
+  static const ADD_PARTICIPATE = '/add-participate';
+  static const VERIFIED_USER = '/verified-user';
+  static const NEW_GROUP_CHAT = '/new-group-chat';
+}
