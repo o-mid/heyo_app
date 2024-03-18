75de43393e4ebc8c5a7d5c654eca3cd97daea94c
omid <omidomirzaei@gmail.com>
2024-03-18 15:53:56 +0330

    added riverpod to chats controller

--- lib/app/modules/chats/controllers/chats_controller.dart
+++ lib/app/modules/chats/controllers/chats_controller.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
-import 'package:get/get.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:heyo/app/modules/chats/data/models/chat_model.dart';
 import 'package:heyo/app/modules/chats/data/repos/chat_history/chat_history_abstract_repo.dart';
@@ -14,17 +14,35 @@ import 'package:heyo/app/modules/new_chat/data/models/user_model/user_model.dart
 import 'package:heyo/app/modules/shared/data/repository/contact_repository.dart';
 import 'package:heyo/app/modules/shared/utils/extensions/core_id.extension.dart';
 
+import '../../messages/data/provider/messages_provider.dart';
+import '../../messages/data/repo/messages_repo.dart';
 import '../../shared/data/models/messaging_participant_model.dart';
 
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../shared/data/providers/database/app_database.dart';
 import '../data/models/chat_view_model/chat_view_model.dart';
 
-class ChatsController extends GetxController {
+final chatsControllerProvider =
+    AutoDisposeAsyncNotifierProvider<ChatsController, List<ChatViewModel>>(
+  () => ChatsController(
+    chatHistoryRepo: Get.find<ChatHistoryLocalAbstractRepo>(),
+    messagesRepo: MessagesRepo(
+      messagesProvider: MessagesProvider(
+        appDatabaseProvider: Get.find<AppDatabaseProvider>(),
+      ),
+    ),
+    contactRepository: Get.find<ContactRepository>(),
+  ),
+);
+
+class ChatsController extends AutoDisposeAsyncNotifier<List<ChatViewModel>> {
   final ChatHistoryLocalAbstractRepo chatHistoryRepo;
   final MessagesAbstractRepo messagesRepo;
   final ContactRepository contactRepository;
+
+  List<ChatViewModel> chats = [];
 
   ChatsController({
     required this.chatHistoryRepo,
@@ -32,96 +50,69 @@ class ChatsController extends GetxController {
     required this.contactRepository,
   });
 
-  final animatedListKey = GlobalKey<AnimatedListState>();
-  late StreamSubscription _chatsStreamSubscription;
-  final chats = <ChatViewModel>[].obs; // Use ChatViewModel instead of ChatModel
-
   @override
-  void onInit() {
-    init();
-    super.onInit();
+  FutureOr<List<ChatViewModel>> build() async {
+    unawaited(_fetchInitialChats());
+    await listenToChatsUpdates();
+    return chats;
   }
 
-  Future<void> init() async {
-    chats.clear();
-    var chatModels = await chatHistoryRepo.getAllChats();
-    chats
-      ..assignAll(chatModels.map((chatModel) => chatModel.toViewModel()).toList())
-      ..sort((a, b) => b.timestamp.compareTo(a.timestamp));
-    await listenToChatsStream();
+  Future<void> _fetchInitialChats() async {
+    final chats = await chatHistoryRepo.getAllChats();
+
+    state = AsyncData(chats.map((chat) => chat.toViewModel()).toList());
   }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    _chatsStreamSubscription.cancel();
-    super.onClose();
-  }
-
-  Future<void> listenToChatsStream() async {
+  Future<void> listenToChatsUpdates() async {
     final chatsStream = await chatHistoryRepo.getChatsStream();
-    _chatsStreamSubscription = chatsStream.listen((newChats) {
-      List<ChatViewModel> removed = [];
-      List<ChatViewModel> added = [];
+    chatsStream.listen((newChats) {
+      List<ChatViewModel> updatedChats = [];
+      Map<String, ChatViewModel> updatedChatsMap = {};
 
       for (final chatModel in newChats) {
         var chatViewModel = chatModel.toViewModel();
-        if (!chats.any((element) => element.id == chatViewModel.id)) {
-          added.add(chatViewModel);
+        updatedChatsMap[chatViewModel.id] = chatViewModel;
+      }
+
+      // Check for removed chats
+      chats.removeWhere((existingChat) => !updatedChatsMap.containsKey(existingChat.id));
+
+      // Update existing chats or add new chats
+      for (var chatId in updatedChatsMap.keys) {
+        var existingChatIndex = chats.indexWhere((chat) => chat.id == chatId);
+        if (existingChatIndex != -1) {
+          chats[existingChatIndex] = updatedChatsMap[chatId]!;
         } else {
-          int index = chats.indexWhere((element) => element.id == chatViewModel.id);
-          chats[index] = chatViewModel;
+          chats.add(updatedChatsMap[chatId]!);
         }
       }
 
-      for (var i = chats.length - 1; i >= 0; i--) {
-        final chatViewModel = chats[i];
-        if (!newChats.any((newChatModel) => newChatModel.id == chatViewModel.id)) {
-          removed.add(chatViewModel);
-        }
-      }
+      // Sort chats by timestamp, newest first
+      chats.sort((a, b) => b.timestamp.compareTo(a.timestamp));
 
-      if (onChatsUpdated != null) {
-        onChatsUpdated!(removed, added);
-      }
+      state = AsyncData(chats);
     });
-
-    chats
-      ..sort((a, b) => b.timestamp.compareTo(a.timestamp))
-      ..refresh();
-  }
-
-  // the callback to be called when chats are updated sent to the view
-  void Function(List<ChatViewModel> removed, List<ChatViewModel> added)? onChatsUpdated;
-
-  Future<void> deleteChat(String chatId) async {
-    await chatHistoryRepo.deleteChat(chatId);
-    await messagesRepo.deleteAllMessages(chatId);
-  }
-
-  Future<bool> showDeleteChatDialog(String chatId) async {
-    await Get.dialog<bool>(
-      DeleteChatDialog(
-        deleteChat: () => deleteChat(chatId),
-      ),
-    );
-    return false;
   }
 
   void showDeleteAllChatsBottomSheet() {
-    openDeleteAllChatsBottomSheet(
-      onDelete: () async {
-        for (final element in chats) {
-          await messagesRepo.deleteAllMessages(element.id);
-        }
-        await chatHistoryRepo.deleteAllChats();
+//     openDeleteAllChatsBottomSheet(
+//       onDelete: () async {
+//         for (final element in chats) {
+//           await messagesRepo.deleteAllMessages(element.id);
+//         }
+//         await chatHistoryRepo.deleteAllChats();
 
-        chats.clear();
-      },
-    );
+//         chats.clear();
+//       },
+//     );
+
+    Future<void> showDeleteAllChatsBottomSheet() async {}
+
+    Future<void> deleteChat(String chatId) async {
+      await chatHistoryRepo.deleteChat(chatId);
+      await messagesRepo.deleteAllMessages(chatId);
+
+      chats.removeWhere((chat) => chat.id == chatId);
+    }
   }
 }
