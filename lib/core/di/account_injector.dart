510a7f0b4dd6b5ed3a3b66d63e99202fdf10d9e3
ali.azimoshan <ali.azimoshan@wallit.eu>
2024-03-19 17:46:49 +0330

    remove all unUsed import

--- lib/core/di/account_injector.dart
+++ lib/core/di/account_injector.dart
@@ -1,11 +1,8 @@
 import 'package:heyo/app/modules/account/controllers/account_controller.dart';
 import 'package:heyo/app/modules/p2p_node/data/key/web3_keys.dart';
-import 'package:heyo/app/modules/shared/data/providers/database/app_database.dart';
-import 'package:heyo/app/modules/shared/data/providers/database/dao/user_provider.dart';
 import 'package:heyo/app/modules/shared/data/repository/account/account_repository.dart';
 import 'package:heyo/app/modules/shared/data/repository/account/app_account_repository.dart';
 import 'package:heyo/app/modules/shared/data/repository/contact_repository.dart';
-import 'package:heyo/app/modules/shared/data/repository/db/cache_repository.dart';
 import 'package:heyo/app/modules/shared/providers/account/creation/account_creation.dart';
 import 'package:heyo/app/modules/shared/providers/account/creation/libp2p_account_creation.dart';
 import 'package:heyo/core/di/injector_provider.dart';
