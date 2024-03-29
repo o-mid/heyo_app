65f543851e66f561c90f865948d8eb7285500f17
ali.azimoshan <ali.azimoshan@wallit.eu>
2024-03-29 14:10:30 +0330

    remove warnings

--- lib/modules/features/call_history/di/call_history_detail_binding.dart
+++ lib/modules/features/call_history/di/call_history_detail_binding.dart
@@ -1,13 +1,12 @@
 import 'package:get/get.dart';
 import 'package:heyo/app/modules/calls/shared/data/providers/call_history/call_history_provider.dart';
-import 'package:heyo/modules/features/call_history/data/local_call_history_repo.dart';
-import 'package:heyo/app/modules/calls/usecase/contact_availability_use_case.dart';
 import 'package:heyo/app/modules/calls/usecase/contact_name_use_case.dart';
 import 'package:heyo/app/modules/shared/data/providers/database/app_database.dart';
 import 'package:heyo/app/modules/shared/data/providers/database/dao/user_provider.dart';
-import 'package:heyo/modules/features/contact/data/local_contact_repo.dart';
 import 'package:heyo/app/modules/shared/data/repository/db/cache_repository.dart';
+import 'package:heyo/modules/features/call_history/data/local_call_history_repo.dart';
 import 'package:heyo/modules/features/call_history/presentation/controllers/call_history_detail_controller.dart';
+import 'package:heyo/modules/features/contact/data/local_contact_repo.dart';
 
 class CallHistoryDetailBinding extends Bindings {
   @override
